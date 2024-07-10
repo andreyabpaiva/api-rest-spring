da052bb058ff68802dcf05ebb9b26a21d13bc8e1
andreyabpaiva <andreyacbpaiva@gmail.com>
2024-07-10 12:00:03 -0300

    add: especificações de acesso e customização das exceptions

--- src/main/java/com/projects/apirest/controllers/UserController.java
+++ src/main/java/com/projects/apirest/controllers/UserController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+import static org.springframework.http.ResponseEntity.status;
+
 @RestController
 @RequestMapping(value="/users")
 public class UserController {
@@ -22,7 +24,7 @@ public class UserController {
     }
 
     @PostMapping
-    public User createUser(@RequestBody User user) {
+    public User createUser(@RequestBody User user){
         return userRepository.save(user);
     }
 
